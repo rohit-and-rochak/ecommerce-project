b7962d4d80a7aae8e4132458b10ddaae1f1d8d24
Rohit Sharma <200002@cs.du.ac.in>
2021-09-22 12:32:00 +0530

    add settings and urls for web app

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import environ
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -59,7 +60,9 @@ ROOT_URLCONF = 'ecommerce.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'web/templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
