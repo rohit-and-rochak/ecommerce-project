e68b7fe014ab2ee46abe95392a6cf6b6590cff32
Rohit Sharma <200002@cs.du.ac.in>
2021-09-23 21:40:19 +0530

    setup base.html file as a template for other html files

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -34,13 +34,14 @@ ALLOWED_HOSTS = [host.strip() for host in env.str("ALLOWED_HOSTS").split(',')]
 # Application definition
 
 INSTALLED_APPS = [
-    'base.apps.BaseConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'base',
+    'web',
 ]
 
 MIDDLEWARE = [
