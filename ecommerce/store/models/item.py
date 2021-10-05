cad807b2f25bc414956cf2bd1b912406ff76a658
Rohit Sharma <200002@cs.du.ac.in>
2021-10-05 09:48:57 +0530

    add units field to item model

--- ecommerce/store/models/item.py
+++ ecommerce/store/models/item.py
@@ -16,8 +16,8 @@ class Item(BaseModel):
         LITRE = 'LITRE'
 
     name        = models.CharField(max_length=100)                                                                              # noqa E221
-    category    = models.CharField(max_length=20, choices=Category.choices, default=Category.GENERIC, null=False, blank=True)  # noqa E221
-    description = models.TextField(max_length=500, blank=True, default=True)                                                    # noqa E221
+    category    = models.CharField(max_length=20, choices=Category.choices, default=Category.GENERIC, null=False, blank=True)   # noqa E221
+    description = models.TextField(max_length=500, blank=True, null=True)                                                       # noqa E221
     discount    = models.PositiveSmallIntegerField(default=0, null=False, blank=True)                                           # noqa E221
     price       = models.DecimalField(max_digits=10, decimal_places=2)                                                          # noqa E221
     quantity    = models.DecimalField(max_digits=4, decimal_places=2, default=0)                                                # noqa E221
