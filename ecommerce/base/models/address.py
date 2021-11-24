e189d60cf4a0f8dd1bed637746759a0f99ec5831
Rohit Sharma <200002@cs.du.ac.in>
2021-11-24 19:22:37 +0530

    add migration files for changes in models

---
+++ ecommerce/base/models/address.py
@@ -0,0 +1,11 @@
+import uuid
+
+from django.db import models
+from .user import User
+
+from address.models import Address as django_address
+
+
+class Address(django_address):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    is_default = models.BooleanField(default=False)
