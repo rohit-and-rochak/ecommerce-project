b490de6ae784260551fa32d494c9f37e3bdbf30f
Rohit Sharma <200002@cs.du.ac.in>
2021-11-27 19:50:43 +0530

    refer cart function from cart view

--- ecommerce/web/urls.py
+++ ecommerce/web/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
 
-from .views import website
+from .views import website, cart
 
 urlpatterns = [
     path('', website.home, name='home'),
     path('shop/', website.shop, name='shop'),
     path('about/', website.about, name='about'),
-    path('cart/', website.cart, name='cart'),
     path('checkout/', website.checkout, name='checkout'),
     path('product/<uuid:product_id>/', website.product, name='product'),
     path('logout/', website.logout_user, name="logout"),
     path('profile/', website.profile, name="profile"),
+
+    path('cart/', cart.cart, name='cart'),
 ]
