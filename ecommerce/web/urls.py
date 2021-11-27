0a06f68e01c554a04b6dee0b077b79a1d306485f
Rohit Sharma <200002@cs.du.ac.in>
2021-11-27 19:49:48 +0530

    remove cart function from website view

--- ecommerce/web/urls.py
+++ ecommerce/web/urls.py
@@ -8,8 +8,7 @@ urlpatterns = [
     path('about/', website.about, name='about'),
     path('cart/', website.cart, name='cart'),
     path('checkout/', website.checkout, name='checkout'),
-    path('product/<uuid:pk>/', website.product, name='product'),
+    path('product/<uuid:product_id>/', website.product, name='product'),
     path('logout/', website.logout_user, name="logout"),
     path('profile/', website.profile, name="profile"),
-
 ]
